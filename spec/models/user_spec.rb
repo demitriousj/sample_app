ac92da7431ea89177ab4f9313d08693a6ac2975d
dmjsc <alphajackson3@gmail.com>
2013-09-09 16:23:19 -0400

    at listing 6.27, just commented out has_secure_password so test will fail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,17 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -38,6 +42,19 @@ describe User do
       end
     end
   end
+
+describe "when password is not present" do
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+  end
+  it { should_not be_valid }
+end
+
+describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatch" }
+  it { should_not be_valid }
+end
 
   describe "when email format is valid" do
     it "should be valid" do
