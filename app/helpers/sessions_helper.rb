c8f5934832579c29618029826ac3e33fa4449e9a
dmjsc <alphajackson3@gmail.com>
2014-01-09 10:36:14 -0500

    Finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 	def sign_in(user)
-    remember_token = User.new_remember_token
+     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
@@ -17,5 +17,12 @@ module SessionsHelper
 
   def signed_in?
     !current_user.nil?
+  end
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.encrypt(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 end
